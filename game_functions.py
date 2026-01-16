368debe1a727135415d8cb69f6d2122f24391dad
sg1041 <sg1041@exeter.ac.uk>
2026-01-16 10:24:20 +0000

    finish game3.py

--- game_functions.py
+++ game_functions.py
@@ -12,6 +12,19 @@ def check_higher_lower(current_val, next_val, user_input):
     else:
         return False
 
+result = False
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
-    pass
+    result = False
+    for i in range(0,len(board)-1):
+        if letter == word[i]:
+            result = True
+            board[i] = letter
+
+    if result == True:
+        print(f'Well done! \'{letter}\' is in the word')
+        return True
+    else:
+        print((f'Sorry, \'{letter}\' is not in the word'))
+        return False
+
